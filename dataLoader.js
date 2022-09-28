0c9282606761cdbad8a208c7bb9dc09c6b18cfeb
jvuorine <vuorinen.jere@gmail.com>
2022-09-28 16:46:30 +0300

    add headers

--- dataLoader.js
+++ dataLoader.js
@@ -1,17 +1,27 @@
 import { data } from "./data.js";
 
+const map = new Map();
+map.set("organizer", "Järjestäjä");
+map.set("name", "Tapahtuman nimi");
+map.set("date", "Päivämäärä");
+map.set("location", "Tapahtumapaikka");
+
 const body = document.getElementById("tableDiv");
 const table = document.createElement("table");
 
 console.log(data)
 
 data.forEach((e) => {
-    const tr = table.insertRow();
-    const td = tr.insertCell();
-    td.appendChild(document.createTextNode(e.organizer));
-    td.appendChild(document.createTextNode(e.name));
-    td.appendChild(document.createTextNode(e.date));
-    td.appendChild(document.createTextNode(e.location));
+    const thead = table.appendChild(document.createElement("thead"));
+    const tbody = table.appendChild(document.createElement("tbody"));
+    const tr = tbody.insertRow();
+    const trh = thead.insertRow();
+    for(const [key, value] of Object.entries(e)){
+        const td = tr.insertCell();
+        const th = trh.insertCell();
+        td.appendChild(document.createTextNode(value));
+        th.appendChild(document.createTextNode(map.get(key)));
+    }
 })
 
 body.appendChild(table);
