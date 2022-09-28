015e4d86aae7555e36d068a6b23931a6ecc9c383
jvuorine <vuorinen.jere@gmail.com>
2022-09-28 16:23:53 +0300

    fix table creator

--- dataLoader.js
+++ dataLoader.js
@@ -3,12 +3,15 @@ import { data } from "./data.js";
 const body = document.getElementById("tableDiv");
 const table = document.createElement("table");
 
+console.log(data)
+
 data.forEach((e) => {
     const tr = table.insertRow();
-    e.forEach((el)=>{
-        const td = tr.insertCell();
-        td.appendChild(document.createTextNode(el));
-    })
+    const td = tr.insertCell();
+    td.appendChild(document.createTextNode(e.organizer));
+    td.appendChild(document.createTextNode(e.name));
+    td.appendChild(document.createTextNode(e.date));
+    td.appendChild(document.createTextNode(e.location));
 })
 
 body.appendChild(table);
